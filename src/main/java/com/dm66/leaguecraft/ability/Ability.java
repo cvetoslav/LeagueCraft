308c443e5f5bb022607b6a1b06cfdda6d5f565b4
dfghj fgyhuj r5tyuio <ceco552@gmail.com>
2021-12-15 21:52:33 +0200

    Added separated item model and fixed rendering issues.

--- src/main/java/com/dm66/leaguecraft/ability/Ability.java
+++ src/main/java/com/dm66/leaguecraft/ability/Ability.java
@@ -1,11 +1,13 @@
 package com.dm66.leaguecraft.ability;
 
 import com.dm66.leaguecraft.Summoner;
+import com.dm66.leaguecraft.item.ability_items.AbilityItem;
 
 public interface Ability
 {
     public double coolDown = 0;
     public boolean isUltimate = false;
+    public AbilityItem item = null;
 
     public void cast(Summoner summoner);
 
